590328382c46f74d78b25cf2bcf0f4b648b2f95d
eternalrecurrenceofthesame <126387487+eternalrecurrenceofthesame@users.noreply.github.com>
2023-06-20 17:53:37 +0900

    Update ByeController.java

--- part2/ch05/ex05/mode_inheritablethredlocal/controller/ByeController.java
+++ part2/ch05/ex05/mode_inheritablethredlocal/controller/ByeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
-@EnableAsync
 public class ByeController {
 
     @GetMapping("/hello")
